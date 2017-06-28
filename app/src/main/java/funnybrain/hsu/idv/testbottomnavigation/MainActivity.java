070e4fb8b9447ee1ef4878185fb1f450c6ab24a7
Hsu, Hsiao-Hsuan <water-resist@yahoo.com.tw>
2017-06-28 16:57:53 +0800

    [feature] try to use collapsing layout

--- app/src/main/java/funnybrain/hsu/idv/testbottomnavigation/MainActivity.java
+++ app/src/main/java/funnybrain/hsu/idv/testbottomnavigation/MainActivity.java
@@ -3,22 +3,34 @@ package funnybrain.hsu.idv.testbottomnavigation;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import funnybrain.hsu.idv.testbottomnavigation.home.HomeFragment;
+
 public class MainActivity extends BaseActivity {
 
-    private TextView mTextMessage;
+    @BindView(R.id.message) TextView mTextMessage;
+    @BindView(R.id.navigation) BottomNavigationView mNavigation;
+
+    private FragmentManager mFragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction ft = mFragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    ft.replace(R.id.content, HomeFragment.newInstance());
+                    ft.commit();
                     return true;
                 case R.id.navigation_reward:
                     mTextMessage.setText(R.string.title_reward);
@@ -41,13 +53,13 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mFragmentManager = getSupportFragmentManager();
 
-        BottomNavigationViewHelper.removeShiftMode(navigation);
-        navigation.getMenu().getItem(0).setChecked(true);
+        BottomNavigationViewHelper.removeShiftMode(mNavigation);
+        mNavigation.getMenu().getItem(0).setChecked(true);
+        mNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
---
+++ app/src/main/java/funnybrain/hsu/idv/testbottomnavigation/home/HomeFragment.java
@@ -0,0 +1,35 @@
+package funnybrain.hsu.idv.testbottomnavigation.home;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import funnybrain.hsu.idv.testbottomnavigation.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {}
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
